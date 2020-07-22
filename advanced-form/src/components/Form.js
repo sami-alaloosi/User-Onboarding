e5306127a9aa00e92a9c8a979719f33da6ab705b
sami-aloosi <kingxsam@gmail.com>
2020-07-22 13:15:09 -0700

    basic form

--- advanced-form/src/components/Form.js
+++ advanced-form/src/components/Form.js
@@ -5,6 +5,61 @@ import * as yup from "yup"
 
 export default function Form () {
     return (
-        <h2> I'm Form</h2>
+        <form>
+        <div>
+                <label htmlFor="username"> 
+                    Username:
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="Enter Your Username"
+                        // value ={?}
+                        // onChange={callBack}
+                    />
+                </label>
+            </div>
+
+            <div>
+            <label htmlFor="email" >
+                Email:
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter Your Email"
+                    id="email"
+                    // value ={?}
+                    // onChange={callback}
+                 />
+            </label>
+            </div>
+            <div>
+            <label htmlFor="password" >
+                Password:
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter Your Password"
+                    id="password"
+                    // value={?}
+                    // onChange={?}
+                 />
+            </label>
+            </div>
+
+            <div>
+                <label htmlFor="checkbox">
+                Do you agree to the terms and conditions?
+                    <input
+                        type="checkbox"
+                        name="terms"
+                        id="checkbox"
+                        // checked= true or falss
+                        // onChange={?}
+                     />
+                </label>
+            </div>
+            <button>Submit</button>
+        </form>
     )
 }
