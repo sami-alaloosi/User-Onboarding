e6272e12cb0b1e5dacc762c6fda523d4d305f6cd
sami-aloosi <kingxsam@gmail.com>
2020-07-22 12:29:08 -0700

    Set Up The Project

---
+++ advanced-form/src/App.js
@@ -0,0 +1,17 @@
+import React,{useState, useEffect} from 'react';
+import axios from "axios"
+import * as yup from "yup"
+import Form from "./components/Form"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+      <h1>sami's app</h1>
+      <Form />
+    </div>
+  );
+}
+
+
---
+++ advanced-form/src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react"
+import * as yup from "yup"
+
+
+
+export default function Form () {
+    return (
+        <h2> I'm Form</h2>
+    )
+}
