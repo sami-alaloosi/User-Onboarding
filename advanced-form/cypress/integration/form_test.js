6d408d0ef8a908b79833ec9c7fb6568ec7995a28
sami-aloosi <kingxsam@gmail.com>
2020-07-23 14:03:02 -0700

    email testing

---
+++ advanced-form/cypress/integration/form_test.js
@@ -0,0 +1,27 @@
+describe('Get the Name input and type a name in it', () =>{
+
+it('can navigate to the site', ()=>{
+    cy.visit('http://localhost:3000/?username=rewrewrwe&email=kingxsam%40gmail.com&password=ewewewe&terms=on')
+    cy.url().should('include', 'username' )
+})
+
+it('typing in the Name input and checking the input', ()=>{
+    cy.get('input[name="username"]').type('sami alaloosi')
+    .should('have.value', "sami alaloosi")
+})
+} )
+
+
+describe('Get the Email input and type an email address in it', () =>{
+    it('can navigate to the site', ()=>{
+        cy.visit('http://localhost:3000/?username=rewrewrwe&email=kingxsam%40gmail.com&password=ewewewe&terms=on')
+    })
+
+    it("typing in the Email input and checking the input", ()=>{
+        cy.get('input[name="email"]').type("kingxsam@gmail.com")
+        .should('have.value', 'kingxsam@gmail.com' )
+    })
+})
+
+
+describe()
